2f2524b3da3eb9041811f0ed8dc1e996a98bf9e9
Michal Jakubczak <michal.jakubczak@ardigen.com>
2020-08-23 18:40:32 +0200

    Add demo app 2

--- R/demo.R
+++ R/demo.R
@@ -1,3 +1,4 @@
+# nocov start
 #' FutureManager demo app
 #' 
 #' Runs the demo app that shows the abilities of the package
@@ -6,10 +7,27 @@
 #' @param port app port
 #' @param launch.browser logical, should launch the browser?
 #' @export
-# nocov start
 demo <- function(host = "0.0.0.0", port = 3838L, launch.browser = FALSE){
   shiny::runApp(
     appDir = system.file("demoapp", package = "FutureManager"),
+    host = host,
+    port = port,
+    launch.browser = launch.browser
+  )
+}
+
+#' FutureManager demo app without the run button
+#' 
+#' Runs another demo app, that doesn't use run button (heavy calculations start 
+#' on-demand, without additional user action)
+#' 
+#' @param host host IP
+#' @param port app port
+#' @param launch.browser logical, should launch the browser?
+#' @export
+demo_noButton <- function(host = "0.0.0.0", port = 3838L, launch.browser = FALSE){
+  shiny::runApp(
+    appDir = system.file("demoapp2", package = "FutureManager"),
     host = host,
     port = port,
     launch.browser = launch.browser
